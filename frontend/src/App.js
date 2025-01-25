a920d648252f70e97b6661e056e9321d03ee2408
Basha-dude <rebelbasha99@gmail.com>
2025-01-25 13:05:43 +0530

    Add basic functionality for Enroll feature not advanced

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,88 @@
 import './App.css';
+import Navbar from './components/Navbar';  
+import { ethers } from "ethers";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import  Create  from "./components/Create";
+import Enroll from './components/Enroll';
+//config.json file is exporting an object as the default export, not named exports.
+import config from "./abi/config.json"; // Import the entire config object
+import FreeLanceDao from "./abi/contracts/FreeLanceDAO.sol/FreeLanceDAO.json"
 
+const { GovernanceTokenAddress, timeLockAddress, myGovernorAddress, freeLanceDAOAddress } = config; 
+
+
+
+/* created minimum components ,should create as required */
 function App() {
+  const [wallet, setwallet] = useState("")
+  const [contract, setContract] = useState("")
+  const [freelancerDetails, setfreelancerDetails] = useState({
+    name:'',
+    skills:'',
+    bio:'',
+    amount:'',
+    isUsd:''
+
+  })
+
+  const FreelanceAddress = freeLanceDAOAddress
+  const FreelanceAddressAbi = FreeLanceDao.abi
+
+  const connectWallet = async() => {
+    console.log("wallet");
+    try {
+      if (window.ethereum) {
+     const accounts =   await window.ethereum.request({
+      "method": "eth_requestAccounts",
+      "params": [],
+     });
+     const provider = new ethers.BrowserProvider(window.ethereum)
+     const signer = await provider.getSigner()
+     const contractInstance =  new ethers.Contract(FreelanceAddress,FreelanceAddressAbi,signer)
+     setContract(contractInstance)
+
+     setwallet(accounts[0])
+     console.log(accounts[0]);
+     
+    
+         }    
+    } catch (error) {
+       console.log(error);
+       
+    }   
+  }
+
+
+  const enrollFreelancer = async() => {
+     console.log("contract",contract); 
+     setfreelancerDetails(freelancerDetails)
+     if (freelancerDetails) {
+      console.log("freelancerDetails",freelancerDetails);
+      
+     }
+
+  }
   return (
+    <Router>
     <div className="App">
-     hello world
+    <h2>FreeLance DAO world </h2>
     </div>
+      <Navbar connectWallet={connectWallet} wallet={wallet} />
+    <Routes>
+    
+     
+     <Route path='/create' element={<Create />} ></Route>
+     <Route path='/enroll' element={<Enroll enrollFreelancer={enrollFreelancer} 
+     setfreelancerDetails={setfreelancerDetails} 
+     freelancerDetails={freelancerDetails} />}></Route>
+  
+   
+
+    
+    </Routes>
+   
+    </Router>
   );
 }
 
